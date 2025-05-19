1aedb8c5ff5ceab5e21e6f778a7893456523c635
Augusto Nicola <auguston64@gmail.com>
2025-05-18 21:30:04 -0300

    RandyBot ahora puede realizar todas las acciones

--- src/administrador/administrador_de_juego.py
+++ src/administrador/administrador_de_juego.py
@@ -2,6 +2,97 @@ from .enums import Acción
 from juego.juego import EstadoDelJuego
 from bots.randy import RandyBot
 
+def faseDeRobo():
+	#* ============================ Fase de robo ============================
+	acciónDeRobo = jugadores[juego.deQuienEsTurno].decidirAcciónDeRobo()
+	
+	if acciónDeRobo == Acción.Robo.DEL_MAZO:
+		# Robar del mazo
+		opcionesDeRobo = juego.robarDelMazo()
+		(indiceDeCartaARobar, indiceDePilaDondeDescartar) = jugadores[juego.deQuienEsTurno].decidirCómoRobarDelMazo(opcionesDeRobo)
+		cartaRobada = juego.elegirRoboDelMazo(indiceDeCartaARobar, indiceDePilaDondeDescartar)
+		print(f"Roba del mazo una {cartaRobada}")
+		
+	elif acciónDeRobo == Acción.Robo.DEL_DESCARTE_0:
+		cartaRobada = juego.robarDelDescarte(0)
+		print(f"Roba del descarte 0 una {cartaRobada}")
+	elif acciónDeRobo == Acción.Robo.DEL_DESCARTE_1:
+		cartaRobada = juego.robarDelDescarte(1)
+		print(f"Roba del descarte 1 una {cartaRobada}")
+	else:
+		#! ERROR
+		raise Exception("Error")
+
+def faseDeDúos():
+	#* ============================ Fase de dúos ============================
+	noSeQuierenJugarMásDúos = False
+	
+	while not noSeQuierenJugarMásDúos and not juego.haTerminado:
+		(acciónDeDúos, cartasAJugar, parametrosDelDúo) = jugadores[juego.deQuienEsTurno].decidirAcciónDeDúos()
+		if acciónDeDúos == Acción.Dúos.JUGAR_PECES:
+			# Jugar dúo de peces
+			cartaRobada = juego.jugarDuoDePeces(cartasAJugar)
+			print(f"Juega un dúo de {list(cartasAJugar.elements())[0]} y {list(cartasAJugar.elements())[1]} y roba una {cartaRobada}")
+			
+		elif acciónDeDúos == Acción.Dúos.JUGAR_BARCOS:
+			# Jugar dúo de barcos
+			print(f"Juega un dúo de {list(cartasAJugar.elements())[0]} y {list(cartasAJugar.elements())[1]}")
+			juego.jugarDuoDeBarcos(cartasAJugar)
+			faseDeRobo()
+			faseDeDúos()
+			noSeQuierenJugarMásDúos = True
+			
+		elif acciónDeDúos == Acción.Dúos.JUGAR_CANGREJOS:
+			# Jugar dúo de cangrejos
+			(pilaDeDescarteARobar, indiceDeCartaARobar) = parametrosDelDúo
+			cartaRobada = juego.jugarDuoDeCangrejos(cartasAJugar, pilaDeDescarteARobar, indiceDeCartaARobar)
+			print(f"Juega un dúo de {list(cartasAJugar.elements())[0]} y {list(cartasAJugar.elements())[1]} para robar una {cartaRobada}")
+			
+		elif acciónDeDúos == Acción.Dúos.JUGAR_NADADOR_Y_TIBURÓN:
+			# Jugar dúo de nadador y tiburón
+			jugadorARobar = parametrosDelDúo[0]
+			cartaRobada = juego.jugarDuoDeNadadorYTiburón(cartasAJugar, jugadorARobar)
+			print(f"Juega un dúo de {list(cartasAJugar.elements())[0]} y {list(cartasAJugar.elements())[1]} para robarle al jugador {jugadorARobar}, y roba una {cartaRobada}")
+			
+		elif acciónDeDúos == Acción.Dúos.NO_JUGAR:
+			# No jugar dúos
+			noSeQuierenJugarMásDúos = True
+			print("No juega ningún dúo")
+			
+		else:
+			#! ERROR
+			raise Exception("Error")
+
+def faseDeFin():
+	#* ============================ Fase de fin de ronda ============================
+	if juego.haTerminado:
+		#* Cuatro sirenas!
+		print("################### CUATRO SIRENAS ###################")
+		print(f"Ganador: {juego.ganador}")
+		for j in range(juego.cantidadDeJugadores):
+			print(f"Jugador {j}: +{juego.estadoDelJugador[j].puntajeDeRonda()} ({juego.puntajesDeJuego[j]}/{juego.puntajeParaGanar()})")
+		print("######################################################")
+	else:
+	
+		acciónDeRobo = jugadores[juego.deQuienEsTurno].decidirAcciónDeFinDeRonda()
+
+		if acciónDeRobo == Acción.FinDeRonda.PASAR_TURNO:
+			# Pasar el turno normalmente
+			juego.pasarTurno()
+			print("Pasa de turno")
+		elif acciónDeRobo == Acción.FinDeRonda.DECIR_BASTA:
+			# Decir basta y terminar la ronda
+			juego.decirBasta()
+			print("¡¡¡Basta!!!")
+		elif acciónDeRobo == Acción.FinDeRonda.DECIR_ÚLTIMA_CHANCE:
+			# Decir última chance y pasar el turno
+			juego.decirÚltimaChance()
+			print("¡¡¡Última Chance!!!")
+		else:
+			#! ERROR
+			raise Exception("Error")
+
+
 juego = EstadoDelJuego(cantidadDeJugadores=2)
 jugadores = [RandyBot(juego, 0), RandyBot(juego, 1)]
 
@@ -14,84 +105,13 @@ while not juego.haTerminado:
 	while juego.rondaEnCurso:
 		print(f"~~~~~~~~~~~~~~~~~~~ Turno del jugador {juego.deQuienEsTurno} ~~~~~~~~~~~~~~~~~~~~")
 		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-		
-		#* ============================ Fase de robo ============================
-		acciónDeRobo = jugadores[juego.deQuienEsTurno].decidirAcciónDeRobo()
-
-		if acciónDeRobo == Acción.Robo.DEL_MAZO:
-			# Robar del mazo
-			opcionesDeRobo = juego.robarDelMazo()
-			(indiceDeCartaARobar, indiceDePilaDondeDescartar) = jugadores[juego.deQuienEsTurno].decidirCómoRobarDelMazo(opcionesDeRobo)
-			cartaRobada = juego.elegirRoboDelMazo(indiceDeCartaARobar, indiceDePilaDondeDescartar)
-			print(f"Roba del mazo una {cartaRobada}")
-			
-		elif acciónDeRobo == Acción.Robo.DEL_DESCARTE_0:
-			cartaRobada = juego.robarDelDescarte(0)
-			print(f"Roba del descarte 0 una {cartaRobada}")
-		elif acciónDeRobo == Acción.Robo.DEL_DESCARTE_1:
-			cartaRobada = juego.robarDelDescarte(1)
-			print(f"Roba del descarte 1 una {cartaRobada}")
-		else:
-			#! ERROR
-			raise Exception("Error")
-		
 		print(f"El descarte 0 es {(juego.descarte[0])}")
 		print(f"El descarte 1 es {(juego.descarte[1])}")
-
-		#* ============================ Fase de dúos ============================
-
-		noSeQuierenJugarMásDúos = False
-
-		while not noSeQuierenJugarMásDúos:
-			acciónDeDúos = acciónDeRobo = jugadores[juego.deQuienEsTurno].decidirAcciónDeDúos()
-			if acciónDeRobo == Acción.Dúos.JUGAR_PECES:
-				# Jugar dúo de peces
-				pass
-			elif acciónDeRobo == Acción.Dúos.JUGAR_BARCOS:
-				# Jugar dúo de barcos
-				pass
-			elif acciónDeRobo == Acción.Dúos.JUGAR_CANGREJOS:
-				# Jugar dúo de cangrejos
-				pass
-			elif acciónDeRobo == Acción.Dúos.JUGAR_NADADOR_Y_TIBURÓN:
-				# Jugar dúo de nadador y tiburón
-				pass
-			elif acciónDeRobo == Acción.Dúos.NO_JUGAR:
-				# No jugar dúos
-				noSeQuierenJugarMásDúos = True
-				print("No juega ningún dúo")
-			else:
-				#! ERROR
-				raise Exception("Error")
-
-		#* ============================ Fase de fin de ronda ============================
-		if juego.haTerminado:
-			#* Cuatro sirenas!
-			print("################### CUATRO SIRENAS ###################")
-			print(f"Ganador: {juego.ganador}")
-			for j in range(juego.cantidadDeJugadores):
-				print(f"Jugador {j}: +{juego.estadoDelJugador[j].puntajeDeRonda()} ({juego.puntajesDeJuego[j]}/{juego.puntajeParaGanar()})")
-			print("######################################################")
-		else:
 		
-			acciónDeRobo = jugadores[juego.deQuienEsTurno].decidirAcciónDeFinDeRonda()
-
-			if acciónDeRobo == Acción.FinDeRonda.PASAR_TURNO:
-				# Pasar el turno normalmente
-				juego.pasarTurno()
-				print("Pasa de turno")
-			elif acciónDeRobo == Acción.FinDeRonda.DECIR_BASTA:
-				# Decir basta y terminar la ronda
-				juego.decirBasta()
-				print("¡¡¡Basta!!!")
-			elif acciónDeRobo == Acción.FinDeRonda.DECIR_ÚLTIMA_CHANCE:
-				# Decir última chance y pasar el turno
-				juego.decirÚltimaChance()
-				print("¡¡¡Última Chance!!!")
-			else:
-				#! ERROR
-				raise Exception("Error")
-
+		faseDeRobo()
+		faseDeDúos()
+		faseDeFin()
+	
 	print("******************** Ronda terminada ********************")
 	for j in range(juego.cantidadDeJugadores):
 		print(f"Jugador {j}: +{juego.estadoDelJugador[j].puntajeDeRonda()} ({juego.puntajesDeJuego[j]}/{juego.puntajeParaGanar()})")
--- src/bots/randy.py
+++ src/bots/randy.py
@@ -1,4 +1,5 @@
 from random import choice
+from collections import Counter as Multiset
 from administrador.enums import Acción
 from juego.carta import Carta
 from juego.juego import EstadoDelJuego, EstadoDeJugador
@@ -31,10 +32,77 @@ class RandyBot():
 			return (indiceDeCartaARobar, indiceDePilaDondeDescartar)
 	
 	def decidirAcciónDeDúos(self):
-		return Acción.Dúos.NO_JUGAR
+		
+		accionesPosibles = [Acción.Dúos.NO_JUGAR]
+		
+		
+		posibleDúoDePeces = self._buscarDúoParaJugar(Carta.Tipo.PEZ)
+		if posibleDúoDePeces != None:
+			accionesPosibles.append(Acción.Dúos.JUGAR_PECES)
+		
+		posibleDúoDeBarcos = self._buscarDúoParaJugar(Carta.Tipo.BARCO)
+		if posibleDúoDeBarcos != None:
+			accionesPosibles.append(Acción.Dúos.JUGAR_BARCOS)
+			
+		posibleDúoDeCangrejos = self._buscarDúoParaJugar(Carta.Tipo.CANGREJO)
+		if posibleDúoDeCangrejos != None and (len(self._juego.descarte[0]) > 0 or len(self._juego.descarte[1]) > 0):
+			accionesPosibles.append(Acción.Dúos.JUGAR_CANGREJOS)
+		
+		posibleDúoDeNadadorYTiburón = self._buscarDúoParaJugar(Carta.Tipo.NADADOR)
+		if posibleDúoDeNadadorYTiburón != None:
+			accionesPosibles.append(Acción.Dúos.JUGAR_NADADOR_Y_TIBURÓN)
+		
+		acciónElegida = choice(accionesPosibles)
+		if acciónElegida == Acción.Dúos.JUGAR_PECES:
+			return (acciónElegida, posibleDúoDePeces, None)
+		
+		elif acciónElegida == Acción.Dúos.JUGAR_BARCOS:
+			return (acciónElegida, posibleDúoDeBarcos, None)
+		
+		elif acciónElegida == Acción.Dúos.JUGAR_CANGREJOS:
+			pilasPosibles = []
+			if (len(self._juego.descarte[0]) > 0):
+				pilasPosibles.append(0)
+			if (len(self._juego.descarte[1]) > 0):
+				pilasPosibles.append(1)
+			pilaElegida = choice(pilasPosibles)
+			indiceElegido = choice(list(range(len(self._juego.descarte[pilaElegida]))))
+			return (acciónElegida, posibleDúoDeCangrejos, (pilaElegida, indiceElegido))
+		
+		elif acciónElegida == Acción.Dúos.JUGAR_NADADOR_Y_TIBURÓN:
+			jugadorARobar = list(range(self._juego.cantidadDeJugadores))
+			del jugadorARobar[self._númeroDeJugador]
+			return (acciónElegida, posibleDúoDeNadadorYTiburón, (jugadorARobar))
+		
+		elif acciónElegida == Acción.Dúos.NO_JUGAR:
+			return (Acción.Dúos.NO_JUGAR, None, None)
+			
 	
 	def decidirAcciónDeFinDeRonda(self):
-		if self._juego.estadoDelJugador[self._númeroDeJugador].puntajeDeRonda() >= 7:
-			return Acción.FinDeRonda.DECIR_BASTA
-		else:
-			return Acción.FinDeRonda.PASAR_TURNO
+		accionesDeFinDeRondaPosibles = [Acción.FinDeRonda.PASAR_TURNO]
+		
+		if self._juego.estadoDelJugador[self._númeroDeJugador].puntajeDeRonda() >= 7 and self._juego.ultimaChancePorJugador == None:
+			accionesDeFinDeRondaPosibles.append(Acción.FinDeRonda.DECIR_BASTA)
+			accionesDeFinDeRondaPosibles.append(Acción.FinDeRonda.DECIR_ÚLTIMA_CHANCE)
+		
+		return choice(accionesDeFinDeRondaPosibles)
+		
+	
+	def _buscarDúoParaJugar(self, tipo):
+		cartasDelDúoEnMano = Multiset([])
+		nadadorEncontrado = False
+		tiburónEncontrado = False
+		for cartaEnMano in self._juego.estadoDelJugador[self._númeroDeJugador].mano.elements():
+			if tipo in [Carta.Tipo.NADADOR, Carta.Tipo.TIBURON]:
+				if (cartaEnMano.tipo == Carta.Tipo.NADADOR and not nadadorEncontrado) or (cartaEnMano.tipo == Carta.Tipo.TIBURON and not tiburónEncontrado):
+					cartasDelDúoEnMano[cartaEnMano] += 1
+					if cartaEnMano.tipo == Carta.Tipo.NADADOR:
+						nadadorEncontrado = True
+					else:
+						tiburónEncontrado = True
+			else: 
+				if cartaEnMano.tipo == tipo:
+					cartasDelDúoEnMano[cartaEnMano] += 1
+			if cartasDelDúoEnMano.total() == 2:
+				return cartasDelDúoEnMano
+		return None
