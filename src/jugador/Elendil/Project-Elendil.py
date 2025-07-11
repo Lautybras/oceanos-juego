d9375ccff9874e08dc7af93a91e80fc02c67c1c5
lautaro bras <lautybras@gmail.com>
2025-07-11 18:19:51 -0300

    no hice nada

--- src/jugador/Elendil/Project-Elendil.py
+++ src/jugador/Elendil/Project-Elendil.py
@@ -9,11 +9,29 @@ class Elendil(JugadorBase):
 	#Que atributos quiero?
 	#Idea, muchisimos atributos y decidir aleatoriamente con respecto a ellos que hacer
 	#Por ahora, colores, winconditions (2, con una no alcanza) cardumen, faro, etc.
-	
+
 
 	# ========================= INTERFAZ DE JUEGO =========================
 	def __init__(self) -> None:
 		super().__init__()
+		self._win_Conditions: int
+		self._tengo_Sirena: bool
+		self._tengo_Cardumen: bool
+		self._tengo_Faro: bool
+		self._tengo_Ancla: bool
+		self._ultima_Chance: bool
+		
+
+
+
+		##Contadores de colores, ojo con el coleccionar colores, reescribir todo arriba y copia
+		##repetir codigo es re diverrrrr, numero 500 a revisar
+		self._ColeccionarColores = max(int(self._tengo_Sirena),  500*int(self._ultima_Chance))
+		self._contadorNegro: int
+		##seguir despeus con todos, hay que ver si los uso
+
+
+
 		#* Acá se pueden hacer más cosas, como definir variables del Bot
 	
 	def configurarParaJuego(self, juego: PartidaDeOcéanos, númeroDeJugador: int, listaDeEventos: list[Evento]) -> None:
